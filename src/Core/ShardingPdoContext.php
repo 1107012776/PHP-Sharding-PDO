85e788512b5be05bb35b3c8c9e4150153da112e5
Yushan Lin <1107012776@qq.com>
2021-09-06 15:20:37 +0800

    非协程上下文管理修改
    非协程上下文管理修改

--- src/Core/ShardingPdoContext.php
+++ src/Core/ShardingPdoContext.php
@@ -117,7 +117,7 @@ class ShardingPdoContext
 
 
     /**
-     * 释放当前上下文，这边,这边释放的是单例上下文，如在常驻内存的情况下，或者workerman框架下
+     * 释放当前上下文，这边,这边释放的是单例上下文，如在常驻内存的情况下，或者workerman框架下 
      * @return boolean
      */
     public static function nonCoroutineContextRelease(){
