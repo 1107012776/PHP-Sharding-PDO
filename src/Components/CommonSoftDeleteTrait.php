a58aa6d0087cae77c3be53656a3e4be34bc847ac
linyushan <linyushan@nawang.cn>
2021-11-08 15:46:15 +0800

    软删除和条件修改

--- src/Components/CommonSoftDeleteTrait.php
+++ src/Components/CommonSoftDeleteTrait.php
@@ -7,21 +7,24 @@
  * @copyright Copyright &copy; 2019-2021
  * @license https://github.com/1107012776/PHP-Sharding-PDO/blob/master/LICENSE
  */
+
 namespace PhpShardingPdo\Components;
 
-class CommonSoftDeleteTrait{
+class CommonSoftDeleteTrait
+{
     protected $softDeleteKey = 'del_flag';  //默认软删除键
 
     /**
      * 可以重写如下自定义软删除返回条件
      * @return array
      */
-    protected function getSoftDeleteCondition(){
+    protected function getSoftDeleteCondition()
+    {
         return [
             $this->softDeleteKey => 0,
-/*          'delete_at' => [
-                'is', null
-            ],*/
+            /*          'delete_at' => [
+                            'is', null
+                        ],*/
         ];
     }
 }
