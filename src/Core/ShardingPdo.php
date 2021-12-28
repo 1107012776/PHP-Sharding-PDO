5d1571360fe2fdd3c5b24cc53016f9a7355fddff
linyushan <linyushan@nawang.cn>
2021-12-28 09:36:57 +0800

    where条件添加验证

--- src/Core/ShardingPdo.php
+++ src/Core/ShardingPdo.php
@@ -152,9 +152,9 @@ class ShardingPdo
     public function where($condition = [])
     {
         foreach ($condition as $key => $val) {
-            if(in_array($key,['more','limit']) //发现是条件关键词，则不允许，不然有注入风险
-            || strpos($key,' ') !== false //条件里面不能存在空格
-            ){
+            if (in_array($key, ['more', 'limit']) //发现是条件关键词，则不允许，不然有注入风险
+                || strpos($key, ' ') !== false //条件里面不能存在空格
+            ) {
                 continue;
             }
             if (!isset($this->_condition[$key])) {
